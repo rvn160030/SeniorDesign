7350dab957088fdce5433be910cce14f072f3262
brian0224 <baotruongvn@gmail.com>
2018-10-03 17:59:49 -0500

    Crash Transaction

---
+++ insurancev1/lib/crashTransaction.js
@@ -0,0 +1,34 @@
+/**
+ * Create car crash transaction
+ * @param {org.seniordesign.vehicle.CreateCrashLog} crashData
+ * @transaction
+ */
+
+ function createCrashLog(crashData) {
+     return getAssetRegistry('org.seniordesign.vehicle.Vehicle')
+        .then(function(crashRegistry){
+            //Add crash
+            var factory = getFactory();
+            var NS = 'org.seniordesign.vehicle';
+
+            var crashID = 'BQT140030';
+            var vehicle = factory.newResource(NS,'Vehicle', crashID);
+
+            vehicle.VIN = crashData.VIN;
+
+            var crashLog = factory.newConcept(NS, "CrashLog");
+
+            crashLog.time = crashData.time;
+            crashLog.speed =  crashData.speed;
+            crashLog.passengers = crashData.passengers;
+            vehicle.crashLog = crashLog;
+            //vehicle.aliasVehicleNumber = [];
+
+            //Emit the event Accident
+           // var event = factory.newEvent(NS, 'CrashLogCreated');
+           // event.crashID= crashID;
+           // emit(event);
+
+            return crashRegistry.addAll([vehicle]);
+        })
+ }
