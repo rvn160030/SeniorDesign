e657b1cc3a9cf7916b72705667cfbbdb8b7de299
Vincent Noble <rvn160030@utdallas.edu>
2018-10-28 19:05:05 -0500

    Added the wepbage stuff Chris made

---
+++ insurancev1/blockChainApp/index.py
@@ -0,0 +1,49 @@
+
+from flask import render_template, Flask, request
+import requests
+
+app = Flask(__name__)
+app.secret_key = "123456"
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/driver', methods=['GET', 'POST'])
+def driver():
+    if request.method == 'POST':
+        url = 'http://localhost:3000/api/org.seniordesign.vehicle.Vehicle'
+        headers = {'Content-type': 'application/json'}
+
+        data = '{"$class": "org.seniordesign.vehicle.Vehicle", "VIN": "9999", "vehcileType": "SUV"}'
+
+        requests.post(url, data=data, headers=headers)
+
+        return render_template('index.html')
+
+
+    return  render_template('driver.html')
+
+@app.route('/police', methods=['GET'])
+def police():
+    url = 'http://localhost:3000/api/org.seniordesign.vehicle.Vehicle'
+    response = requests.get(url)
+    data= response.json()
+
+    return  render_template('police.html', text=data)
+
+@app.route('/insurance', methods=['GET'])
+def insurance():
+    url = 'http://localhost:3000/api/org.seniordesign.vehicle.Vehicle'
+    response = requests.get(url)
+    data = response.json()
+
+    return  render_template('insurance.html', text=data)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+# ====================
