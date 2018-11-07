4c1080bc15fbe80b29199e663b2ce733a41a8ac1
chris <w.chris2711@gmail.com>
2018-11-07 16:01:11 -0600

    Trying again to make the display work in a nice manner part 24

--- insurancev1/blockChainApp/index.py
+++ insurancev1/blockChainApp/index.py
@@ -32,7 +32,7 @@ def driver():
 def police():
     url = 'http://localhost:3000/api/org.seniordesign.vehicle.Vehicle'
     response = requests.get(url)
-    data = json.loads(response.text)
+    data = response.json()
 
 
     return  render_template('police.html', data=data)
