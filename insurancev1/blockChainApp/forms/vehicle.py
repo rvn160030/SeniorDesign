57ad44530fced259612a218e4392591ac059af10
chris <w.chris2711@gmail.com>
2018-11-29 16:01:44 -0600

    Please look good

--- insurancev1/blockChainApp/forms/vehicle.py
+++ insurancev1/blockChainApp/forms/vehicle.py
@@ -1,17 +1,11 @@
-from wtforms import StringField, validators, SelectMultipleField, widgets
+from wtforms import StringField, validators, SelectField
 from flask_wtf import Form
 
-class MultiCheckboxField(SelectMultipleField):
-    widget = widgets.ListWidget(prefix_label=False)
-    option_widget = widgets.CheckboxInput()
 
 
 class VehicleForm(Form):
     vin = StringField('VIN', [validators.Length(min=1, max=10)])
-
-    string_of_types = ['TRUCK\r\nSUV\r\nVAN\r\nMINIVAN\r\nWAGON\r\nSEDAN\r\nCOUPE\r\nCABRIOLET\r\nROADSTER']
-    list_of_types = string_of_types[0].split()
-
-    types = [(x,x) for x in list_of_types]
-    type = MultiCheckboxField('Type', choices=types)
+    type = SelectField('Type', choices=[('TRUCK', 'Truck'), ('SUV', 'Suv'), ('VAN', 'Van'), ('MINIVAN', 'Minivan'),
+                                        ('WAGON', 'Wagon'), ('SEDAN', 'Sedan'), ('COUPE', 'Coupe'), ('CABRIOLET', 'Cabriolet'),
+                                        ('ROADSTER', 'Roadster')])
 
