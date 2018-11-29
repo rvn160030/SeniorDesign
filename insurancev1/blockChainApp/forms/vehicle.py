988dc196b89beae3bec17b6f6c0b9d58183e1805
chris <w.chris2711@gmail.com>
2018-11-29 16:26:11 -0600

    Please look good

--- insurancev1/blockChainApp/forms/vehicle.py
+++ insurancev1/blockChainApp/forms/vehicle.py
@@ -7,5 +7,5 @@ class VehicleForm(Form):
     type = SelectField('Type', choices=[('TRUCK', 'Truck'), ('SUV', 'Suv'), ('VAN', 'Van'), ('MINIVAN', 'Minivan'),
                                         ('WAGON', 'Wagon'), ('SEDAN', 'Sedan'), ('COUPE', 'Coupe'), ('CABRIOLET', 'Cabriolet'),
                                         ('ROADSTER', 'Roadster')])
-    submit1 = SubmitField('Submit')
+    submit1 = SubmitField('')
 
